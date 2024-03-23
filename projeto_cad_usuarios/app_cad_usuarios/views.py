8436739900dc7c43495dd9c460753fc783fee4ad
Elildes F Santos <colafina2000@yahoo.com.br>
2024-03-23 01:26:43 -0300

    create: rota, views, html, models, migrations and database sqlite

--- projeto_cad_usuarios/app_cad_usuarios/views.py
+++ projeto_cad_usuarios/app_cad_usuarios/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):   # acessar dados da página
+     return render(request,'usuarios/home.html')    # acessar página html
+
+def usuarios(request):
+     pass
--- projeto_cad_usuarios/projeto_cad_usuarios/urls.py
+++ projeto_cad_usuarios/projeto_cad_usuarios/urls.py
@@ -14,9 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+#from django.contrib import admin
 from django.urls import path
+from app_cad_usuarios import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # rota, view responsável, nome de referência
+    # usuarios.com
+    path('',views.home,name='home'),
+    # usuarios.com/usuarios
+    path('usuarios/',views.usuarios,name='listagem_usuarios')
 ]
